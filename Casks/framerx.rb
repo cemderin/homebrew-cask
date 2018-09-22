7ee6fec5f618f47fb753af17c9ff9584c5dc5d88
Cem Derin <cd@kodehaus.de>
2018-09-22 20:36:23 +0200

    Add appcast to FramerX Fix version and hash

--- Casks/framerx.rb
+++ Casks/framerx.rb
@@ -1,8 +1,9 @@
 cask 'framerx' do
-  version ':latest'
-  sha256 ':no_check'
+  version :latest
+  sha256 :no_check
 
   url 'https://dl.devmate.com/com.framer.x/FramerX.zip'
+  appcast 'https://updates.devmate.com/com.framer.x.xml'
   name 'FramerX'
   homepage 'https://framer.com/'
 
