3a89f67cfd3e32cc707892618e239870cc26a40e
Cem Derin <cd@kodehaus.de>
2018-09-22 14:07:52 +0200

    Add FramerX latest

---
+++ Casks/framerx.rb
@@ -0,0 +1,10 @@
+cask 'framerx' do
+  version ':latest'
+  sha256 ':no_check'
+
+  url 'https://dl.devmate.com/com.framer.x/FramerX.zip'
+  name 'FramerX'
+  homepage 'https://framer.com/'
+
+  app 'Framer X.app'
+end
